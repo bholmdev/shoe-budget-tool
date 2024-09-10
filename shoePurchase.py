b015b140b1080142636f7147e194637c68d59e34
Brandon Holman <112407387+bholmdev@users.noreply.github.com>
2024-09-09 22:43:39 -0500

    fixed typo in shoe_budget && added if statement

--- shoePurchase.py
+++ shoePurchase.py
@@ -6,8 +6,13 @@ high = Shoes("Off White", 400)
 
 try:
     shoe_budget = float(input("What is your shoe budget?  "))
+
+    if shoe_budget < low.price:
+        print("Sorry, you need to hustle more :(")
+        exit()
+
 except ValueError:
-    exit("Please engter a number.")
+    exit("Please enter a number.")
 
 for shoes in [high, medium, low]:
     shoes.buy(shoe_budget)
